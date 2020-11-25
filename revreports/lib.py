a65358071f4849a759627dde34c5e26b7f1ff5c5
Chris Westerman <chris.westerman8@gmail.com>
2020-11-25 19:46:36 +0000

    moved functions to lib

--- revreports/lib.py
+++ revreports/lib.py
@@ -7,20 +7,50 @@ from os.path import split
 import pandas as pd
 import datetime
 
+ACTUALS = '../revreports/data/Actuals/2020_seg'
+
 pd.set_option('display.width', 200)
 
 
-def convert_seg_stat(data):
-    pass
+def convert_seg_stat(date):
+    # need file name and date to add to report
+    file_date = date.replace('/', '_')
+    df = pd.read_csv(f'../revreports/data/stat/segment_stat_{file_date}.txt', sep="\t", engine='python', skipfooter=2,
+                 usecols=['GRP2_CODE', 'ROOMS_DAY', 'ROOM_REV_DAY'])
 
-def convert_seg_res(data):
-    pass
+    df.columns = ['Code', 'Res', 'Rev']
+    df['Date'] = date
+    df.to_csv(actuals, mode='a', header=False, index=False)
 
-def convert_source_stat(data):
-    pass
 
-def convert_source_res(data):
-    pass
+def convert_seg_res(date):
+    df = pd.read_csv('../revreports/data/11_23_20/segment_res.txt', sep="\t",
+                 usecols=['RESERVATION_DATE', 'MARKET_CODE', 'NO_DEFINITE_ROOMS', 'TOTAL_REVENUE'],
+                engine='python', skipfooter=2)#, parse_dates=['RESERVATION_DATE'])
+
+    df.columns = ['Date', 'Code', 'Res', 'Rev']
+    # where do these get saved
+    df.to_csv(date, index=False)
+
+
+def convert_source_stat(date):
+    file_date = date.replace('/', '_')
+    df = pd.read_csv(f'../revreports/data/stat/source_stat_{file_date}.txt', sep="\t", engine='python', skipfooter=2,
+                 usecols=['GRP2_CODE', 'ROOMS_DAY', 'ROOM_REV_DAY'])
+
+    df.columns = ['Code', 'Res', 'Rev']
+    df['Date'] = date
+    # Saving in it's own location or with segment?
+    df.to_csv(actuals, mode='a', header=False, index=False)
+
+def convert_source_res(date):
+    df = pd.read_csv('../revreports/data/11_23_20/segment_res.txt', sep="\t",
+                 usecols=['RESERVATION_DATE', 'MARKET_CODE', 'NO_DEFINITE_ROOMS', 'TOTAL_REVENUE'],
+                engine='python', skipfooter=2)#, parse_dates=['RESERVATION_DATE'])
+
+    df.columns = ['Date', 'Code', 'Res', 'Rev']
+    # where do these get saved
+    df.to_csv(date, index=False)
 
 
 
